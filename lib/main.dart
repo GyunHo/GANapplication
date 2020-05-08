bc25969a122bb814a53a91362ee43cfa57ce7d9e
diablooo <commandoo@hanmir.com>
2020-05-08 22:36:48 +0900

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,214 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:slider_button/slider_button.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: ImageInputPage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class ImageInputPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _ImageInputPageState createState() => _ImageInputPageState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _ImageInputPageState extends State<ImageInputPage> {
+  File _originImage;
+  File _makeUpImage;
+  StorageReference _storageReference = FirebaseStorage.instance.ref();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    Size _size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: Color(0xfffedbc0),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            SizedBox(
+              height: _size.height * 0.1,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Makeup',
+              style: TextStyle(fontSize: 50.0),
+            ),
+            Text(
+              'Transfer',
+              style: TextStyle(fontSize: 50.0),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                InkWell(
+                  child: DottedBorder(
+                    strokeWidth: 3.0,
+                    dashPattern: [5.0],
+                    borderType: BorderType.RRect,
+                    radius: Radius.circular(12),
+                    padding: EdgeInsets.all(6),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                        child: Container(
+                          child: _originImage != null
+                              ? Image(
+                                  fit: BoxFit.cover,
+                                  image: FileImage(File(_originImage.path)),
+                                )
+                              : Center(child: Text('add Makeup Image')),
+                          width: _size.width * 0.4,
+                          height: _size.height * 0.3,
+                        )),
+                  ),
+                  onTap: () async {
+                    await imageDialog().then((res) {
+                      if (res is File) {
+                        setState(() {
+                          _originImage = res;
+                        });
+                      }
+                    });
+                  },
+                ),
+                InkWell(
+                  child: DottedBorder(
+                    strokeWidth: 3.0,
+                    dashPattern: [5.0],
+                    borderType: BorderType.RRect,
+                    radius: Radius.circular(12),
+                    padding: EdgeInsets.all(6),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                        child: Container(
+                          child: _makeUpImage != null
+                              ? Image(
+                                  fit: BoxFit.cover,
+                                  image: FileImage(File(_makeUpImage.path)),
+                                )
+                              : Center(child: Text('add Makeup Image')),
+                          width: _size.width * 0.4,
+                          height: _size.height * 0.3,
+                        )),
+                  ),
+                  onTap: () async {
+                    await imageDialog().then((res) {
+                      if (res is File) {
+                        setState(() {
+                          _makeUpImage = res;
+                        });
+                      }
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: _size.height * 0.1,
+            ),
+            Center(
+              child: SliderButton(
+                label: Text(
+                  'Slide to makeup',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                buttonColor: Colors.black,
+                backgroundColor: Colors.white,
+                icon: Icon(
+                  Icons.brush,
+                  color: Colors.white,
+                ),
+                dismissible: false,
+                vibrationFlag: false,
+                action: () {
+                  upload();
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Future<File> pickImage(ImageSource source) async {
+    File image = await ImagePicker.pickImage(source: source);
+    return image;
+  }
+
+  Future imageDialog() {
+    return showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+        child: Container(
+          height: 120,
+          width: 180,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.camera_alt,
+                      size: 30.0,
+                    ),
+                    onPressed: () {
+                      pickImage(ImageSource.camera).then((File img) {
+                        Navigator.pop(context, img);
+                      });
+                    },
+                  ),
+                  Text('Camera'),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(
+                      Icons.photo_album,
+                      size: 30.0,
+                    ),
+                    onPressed: () {},
+                  ),
+                  Text('Album')
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  upload() async {
+    StorageReference _reference =  FirebaseStorage.instance
+        .ref().child('img').child('${DateTime.now().millisecondsSinceEpoch.toString()}.jpg');
+    StorageUploadTask task = _reference.putFile(_originImage);
+    await task.onComplete.then((StorageTaskSnapshot snapshot)  async {
+      print( await snapshot.ref.getBucket());
+      print( await snapshot.ref.getName());
+
+    });
+
+    }
+
+
 }
